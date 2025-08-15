60168fbd85e010a0bb0b14ebc6eaeb8e22ea7581
LiZhiQing <3040469570@qq.com>
2025-08-15 10:38:28 +0800

    完成 LearnOpenGL CN 教程的 "光照贴图" 章节...

---
+++ src/chapter/lightingMaps/lightingMaps.cpp
@@ -0,0 +1,165 @@
+#include <OpenGLTools.h>
+#include <filesystem>
+#include <chapter/lightingMaps/Uniform.h>
+using namespace OpenGLTools;
+
+glm::vec3 lightPos(0.0f, 0.0f, 2.0f);
+
+int main()
+{
+    Init& initInstance = Init::getInstance();
+    initInstance.setCamera(Camera(glm::vec3(0.0f, 0.0f, 3.0f))).setScreenHeight(600).setScreenWidth(800).
+                 setTitle("Lighting Maps | 光照贴图").builder();
+
+    GLFWwindow* window = initInstance.getWindow();
+
+    auto lightingShader = Shader("resources/shader/lightingMaps/lightingMaps.vert",
+                                 "resources/shader/lightingMaps/lightingMaps.frag");
+    auto lightCubeShader = Shader("resources/shader/lightingMaps/light_cube.vert",
+                                  "resources/shader/lightingMaps/light_cube.frag");
+
+    const float vertices[] = {
+        // positions          // normals           // texture coords
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f,
+        0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f, 1.0f, 0.0f,
+        0.5f, 0.5f, -0.5f, 0.0f, 0.0f, -1.0f, 1.0f, 1.0f,
+        0.5f, 0.5f, -0.5f, 0.0f, 0.0f, -1.0f, 1.0f, 1.0f,
+        -0.5f, 0.5f, -0.5f, 0.0f, 0.0f, -1.0f, 0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f,
+
+        -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+        0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f,
+        0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,
+        0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,
+        -0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,
+        -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+
+        -0.5f, 0.5f, 0.5f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+        -0.5f, 0.5f, -0.5f, -1.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+        -0.5f, -0.5f, 0.5f, -1.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+        -0.5f, 0.5f, 0.5f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+
+        0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+        0.5f, 0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+        0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+        0.5f, -0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+        0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f, 0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f, 1.0f, 1.0f,
+        0.5f, -0.5f, 0.5f, 0.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+        0.5f, -0.5f, 0.5f, 0.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+        -0.5f, -0.5f, 0.5f, 0.0f, -1.0f, 0.0f, 0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f, 0.0f, 1.0f,
+
+        -0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
+        0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f,
+        0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,
+        0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,
+        -0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f,
+        -0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f
+    };
+
+    GLuint VBO, cubeVAO;
+    glGenVertexArrays(1, &cubeVAO);
+    glGenBuffers(1, &VBO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindVertexArray(cubeVAO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), static_cast<void*>(nullptr));
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), reinterpret_cast<void*>(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), reinterpret_cast<void*>(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    GLuint lightCubeVAO;
+    glGenVertexArrays(1, &lightCubeVAO);
+    glBindVertexArray(lightCubeVAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), static_cast<void*>(nullptr));
+    glEnableVertexAttribArray(0);
+
+    const GLuint diffuseMap = loadTexture(
+        std::filesystem::path("resources/images/container2.png").string().c_str());
+    const GLuint specularMap = loadTexture(
+        std::filesystem::path("resources/images/container2_specular.png").string().c_str());
+
+    uniform.material.diffuse(0);
+    uniform.material.specular(1);
+    uniform.light.ambient(glm::vec3(0.2f, 0.2f, 0.2f));
+    uniform.light.diffuse(glm::vec3(0.5f, 0.5f, 0.5f));
+    uniform.light.specular(glm::vec3(1.0f, 1.0f, 1.0f));
+    uniform.material.shininess(64.0f);
+
+    lightingShader
+        .use()
+        .setInt(uniform.material.diffuse)
+        .setInt(uniform.material.specular);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        const auto currentFrame = static_cast<GLfloat>(glfwGetTime());
+        initInstance.setDeltaTime(currentFrame - initInstance.getLastFrame());
+        initInstance.setLastFrame(currentFrame);
+        processInput(window);
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glm::mat4 projection = glm::perspective(glm::radians(initInstance.getCamera().getZoom()),
+                                                static_cast<float>(initInstance.getScreenWidth()) / static_cast<float>(
+                                                    initInstance.getScreenHeight()), 0.1f, 100.0f);
+        glm::mat4 view = initInstance.getCamera().getViewMatrix();
+        auto model = glm::mat4(1.0f);
+        constexpr float radius = 2.0f; // 旋转半径
+        lightPos.x = static_cast<GLfloat>(sin(glfwGetTime()) * radius);
+        lightPos.z = static_cast<GLfloat>(cos(glfwGetTime()) * radius);
+
+        lightingShader
+            .use()
+            .setVec3(uniform.light.position(lightPos))
+            .setVec3(uniform.viewPos(initInstance.getCamera().getPosition()))
+            .setVec3(uniform.light.ambient)
+            .setVec3(uniform.light.diffuse)
+            .setVec3(uniform.light.specular)
+            .setFloat(uniform.material.shininess)
+            .setMat4(uniform.view(view))
+            .setMat4(uniform.projection(projection))
+            .setMat4(uniform.model(model));
+
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, diffuseMap);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, specularMap);
+
+        glBindVertexArray(cubeVAO);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, lightPos);
+        model = glm::scale(model, glm::vec3(0.2f));
+
+        lightCubeShader
+            .use()
+            .setMat4(uniform.projection)
+            .setMat4(uniform.view)
+            .setMat4(uniform.model(model));
+
+        glBindVertexArray(lightCubeVAO);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glDeleteVertexArrays(1, &cubeVAO);
+    glDeleteVertexArrays(1, &lightCubeVAO);
+    glDeleteBuffers(1, &VBO);
+
+    return 0;
+}
