824cf988b4690d5d97242bdc07ae664bb6de01ba
LiZhiQing <3040469570@qq.com>
2025-08-15 11:25:55 +0800

    完成 LearnOpenGL CN 教程的 "深度测试" 章节...

---
+++ src/chapter/depthTesting/depthTesting.cpp
@@ -0,0 +1,160 @@
+#include <OpenGLTools.h>
+#include <filesystem>
+#include <chapter/depthTesting/Uniform.h>
+namespace fs = std::filesystem;
+using namespace OpenGLTools;
+
+int main()
+{
+    Init& initInstance = Init::getInstance();
+    initInstance
+        .setCamera(Camera(glm::vec3(0.0f, 0.0f, 3.0f)))
+        .setScreenHeight(600)
+        .setScreenWidth(800)
+        .setTitle("Depth Testing | 深度测试")
+        .builder();
+    GLFWwindow* window = initInstance.getWindow();
+
+    constexpr GLfloat cubeVertices[] = {
+        // positions          // texture Coords
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+        0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
+        0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+        0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+        -0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+
+        -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+        0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+        0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
+        0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
+        -0.5f, 0.5f, 0.5f, 0.0f, 1.0f,
+        -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+
+        -0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+        -0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+        -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+        -0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+
+        0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+        0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+        0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+        0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+        0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f, 1.0f, 1.0f,
+        0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+        0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+        -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+
+        -0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
+        0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+        0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+        0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+        -0.5f, 0.5f, 0.5f, 0.0f, 0.0f,
+        -0.5f, 0.5f, -0.5f, 0.0f, 1.0f
+    };
+    constexpr GLfloat planeVertices[] = {
+        // positions          // texture Coords
+        5.0f, -0.5f, 5.0f, 2.0f, 0.0f,
+        -5.0f, -0.5f, 5.0f, 0.0f, 0.0f,
+        -5.0f, -0.5f, -5.0f, 0.0f, 2.0f,
+
+        5.0f, -0.5f, 5.0f, 2.0f, 0.0f,
+        -5.0f, -0.5f, -5.0f, 0.0f, 2.0f,
+        5.0f, -0.5f, -5.0f, 2.0f, 2.0f
+    };
+
+    GLuint cubeVAO, cubeVBO;
+    glGenVertexArrays(1, &cubeVAO);
+    glGenBuffers(1, &cubeVBO);
+    glBindVertexArray(cubeVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), &cubeVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), static_cast<void*>(nullptr));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), reinterpret_cast<void*>(3 * sizeof(GLfloat)));
+    glBindVertexArray(0);
+    // plane VAO
+    GLuint planeVAO, planeVBO;
+    glGenVertexArrays(1, &planeVAO);
+    glGenBuffers(1, &planeVBO);
+    glBindVertexArray(planeVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, planeVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(planeVertices), &planeVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), static_cast<void*>(nullptr));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), reinterpret_cast<void*>(3 * sizeof(GLfloat)));
+    glBindVertexArray(0);
+
+    const GLuint cubeTexture = loadTexture(fs::path("resources/images/marble.jpg").string().c_str());
+    const GLuint floorTexture = loadTexture(fs::path("resources/images/metal.png").string().c_str());
+
+    glEnable(GL_DEPTH_TEST);
+    // glDepthFunc(GL_ALWAYS);
+
+    Shader shader("resources/shader/depthTesting/depthTesting.vert", "resources/shader/depthTesting/depthTesting.frag");
+
+    shader.use().setInt(uniform.texture1(0));
+
+    glfwSetTime(0);
+    while (!glfwWindowShouldClose(window))
+    {
+        const auto currentFrame = static_cast<GLfloat>(glfwGetTime());
+        initInstance.setDeltaTime(currentFrame - initInstance.getLastFrame());
+        initInstance.setLastFrame(currentFrame);
+        processInput(window);
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        Camera camera = initInstance.getCamera();
+
+        auto view = camera.getViewMatrix();
+        auto model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(-1.0f, 0.0f, -1.0f));
+        auto projection = glm::perspective(
+            glm::radians(camera.getZoom()),
+            static_cast<GLfloat>(initInstance.getScreenWidth()) / static_cast<GLfloat>(initInstance.getScreenHeight()),
+            0.1f,
+            100.0f
+        );
+        shader
+            .use()
+            .setMat4(uniform.model(model))
+            .setMat4(uniform.view(view))
+            .setMat4(uniform.projection(projection));
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, cubeTexture);
+        glBindVertexArray(cubeVAO);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(2.0f, 0.0f, 0.0f));
+        shader
+            .use()
+            .setMat4(uniform.model(model));
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        glBindVertexArray(planeVAO);
+        glBindTexture(GL_TEXTURE_2D, floorTexture);
+        shader
+            .use()
+            .setMat4(uniform.model(glm::mat4(1.0f)));
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glDeleteVertexArrays(1, &cubeVAO);
+    glDeleteVertexArrays(1, &planeVAO);
+    glDeleteBuffers(1, &cubeVBO);
+    glDeleteBuffers(1, &planeVBO);
+    return 0;
+}
---
+++ src/lib/include/chapter/depthTesting/Uniform.h
@@ -0,0 +1,14 @@
+#ifndef UNIFORM_H
+#define UNIFORM_H
+#include <packGlm.h>
+#include <string>
+#include <glm/glm.hpp>
+
+inline struct Uniform {
+	packGlm<int> texture1{"texture1"};
+	packGlm<glm::mat4> projection{"projection"};
+	packGlm<glm::mat4> view{"view"};
+	packGlm<glm::mat4> model{"model"};
+} uniform;
+
+#endif
